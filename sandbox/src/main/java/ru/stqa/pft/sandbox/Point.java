3c3165c04e4b022e026f91a6b858215c8c13f137
Irina <i.a.kashapova@rambler.ru>
2018-04-01 23:29:30 +0400

    Выполнение 1 пункта 2 задания. Создание класса Point  для представления точек на двумерной плоскости
    1. Создать класс Point для представления точек на двумерной плоскости. Объекты этого класса должны содержать два атрибута, которые соответствуют координатам точки на плоскости.

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,10 @@
+package ru.stqa.pft.sandbox;
+
+public class Point {
+  public static void main(String[] args) {
+    int a = 5;
+    int b = 6;
+
+    System.out.println("Координаты точки на плоскости = (" + a +";"+ b + ")");
+  }
+}
