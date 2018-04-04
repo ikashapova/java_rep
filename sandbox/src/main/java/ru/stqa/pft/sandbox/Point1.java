9f1de189684b35ba8440573f180524ee442d3add
Irina <i.a.kashapova@rambler.ru>
2018-04-05 01:43:52 +0400

    Ответ на задание 2.3 " Сделать запускаемый класс, то есть содержащий функцию public static void main(String[] args) {...} и при помощи него убедиться, что функция вычисления расстояния между точками действительно работает. Результат вычисления выводить на экран и контролировать визуально.' Сделала как поняла, наверное совсем не верно

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point1.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point1.java
@@ -3,6 +3,12 @@ package ru.stqa.pft.sandbox;
 public class Point1 {
    public double x;
    public double y;
-
+public Point1(double x, double y){
+  this.x=x;
+  this.y=y;
+}
+  public double distance(Point1 b, Point1 c) {
+    return Math.sqrt((c.x - b.x) * (c.x - b.x) + (c.y - b.y) * (c.y - b.y));
+  }
   }
 
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point2.java
+++
@@ -1,7 +0,0 @@
-package ru.stqa.pft.sandbox;
-
-public class Point2 {
-  public double x;
-  public double y;
-  }
-
