6a97dd8202d0e300b2d5ba16d7bbfa8e94711836
Irina <i.a.kashapova@rambler.ru>
2018-04-05 01:50:12 +0400

    Ответ на 2.4 "4. Реализовать то же самое (вычисление расстояния между двумя точками) при помощи метода в классе Point, и добавить в созданный в предыдущем пункте запускаемый класс примеры использования метода вместо ранее созданной функции."

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyPoint.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyPoint.java
@@ -2,13 +2,11 @@ package ru.stqa.pft.sandbox;
 
 public class MyPoint {
   public static void main(String[] args){
-    Point1 s = new Point1(5, 6);
-    Point1 b = new Point1(0, 0);
-    System.out.println("Расстояние между точками = " + distance(s, b));
+    Point1 s = new Point1(5, 6, 0, 0);
+
+    System.out.println("Расстояние между точками = " + s.distance());
 
 
   }
-  public static double distance(Point1 b, Point1 c) {
-    return Math.sqrt((c.x - b.x) * (c.x - b.x) + (c.y - b.y) * (c.y - b.y));
-  }
+
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point1.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point1.java
@@ -3,12 +3,16 @@ package ru.stqa.pft.sandbox;
 public class Point1 {
    public double x;
    public double y;
-public Point1(double x, double y){
+   public double z;
+   public double m;
+public Point1(double x, double y, double z, double m){
   this.x=x;
   this.y=y;
+  this.z=z;
+  this.m=m;
 }
-  public double distance(Point1 b, Point1 c) {
-    return Math.sqrt((c.x - b.x) * (c.x - b.x) + (c.y - b.y) * (c.y - b.y));
+  public double distance() {
+    return Math.sqrt((this.z - this.x) * (this.z - this.x) + (this.m - this.y) * (this.m - this.y));
   }
   }
 
