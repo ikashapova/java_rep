f925715f75ebf6b4f35123847c8beb93ff0a233c
Irina <i.a.kashapova@rambler.ru>
2018-04-10 02:34:23 +0400

    4 задание: создание теста для создания контакта. Рефакторинг теста

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,37 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+  private final String firstname;
+  private final String lastname;
+  private final String address;
+  private final String phon;
+  private final String email;
+
+  public ContactData(String firstname, String lastname, String address, String phon, String email) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.address = address;
+    this.phon = phon;
+    this.email = email;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getPhon() {
+    return phon;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+}
