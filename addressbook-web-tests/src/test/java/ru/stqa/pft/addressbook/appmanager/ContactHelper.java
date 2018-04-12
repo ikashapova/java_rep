ac93a920f4961d1360824da04d2bc26e7a3d59a1
Irina <i.a.kashapova@rambler.ru>
2018-04-13 02:56:46 +0400

    5 задание Создан для  вспомогательных классов общий базовый класс BaseHelper, перенесены в него низкоуровневые вспомогательные методы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -4,11 +4,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.stqa.pft.addressbook.model.ContactData;
 
-public class ContactHelper {
-  private FirefoxDriver wd;
+public class ContactHelper extends HelperBase  {
 
   public ContactHelper(FirefoxDriver wd) {
-    this.wd = wd;
+    super(wd);
   }
 
   public void submitContactCreation() {
@@ -16,24 +15,16 @@ public class ContactHelper {
   }
 
   public void fillContactForm(ContactData contactData) {
-      wd.findElement(By.name("firstname")).click();
-      wd.findElement(By.name("firstname")).clear();
-      wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
-      wd.findElement(By.name("lastname")).click();
-      wd.findElement(By.name("lastname")).clear();
-      wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
-      wd.findElement(By.name("address")).click();
-      wd.findElement(By.name("address")).clear();
-      wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
-      wd.findElement(By.name("home")).click();
-      wd.findElement(By.name("home")).clear();
-      wd.findElement(By.name("home")).sendKeys(contactData.getPhon());
+      type(By.name("firstname"),contactData.getFirstname());
+      type(By.name("lastname"),contactData.getLastname());
+      type(By.name("address"),contactData.getAddress());
+      type(By.name("home"),contactData.getPhon());
+      type(By.name("home"),contactData.getEmail());
       wd.findElement(By.name("email")).click();
       wd.findElement(By.name("email")).clear();
-      wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
-  }
+        }
 
   public void gotoAddContact() {
-      wd.findElement(By.linkText("add new")).click();
+      click(By.linkText("add new"));
   }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -3,18 +3,17 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class NavigationHelper {
-  private FirefoxDriver wd;
+public class NavigationHelper extends HelperBase {
 
   public NavigationHelper(FirefoxDriver wd) {
-    this.wd = wd;
+    super(wd);
   }
 
   public void gotoGroup() {
-      wd.findElement(By.linkText("groups")).click();
+      click(By.linkText("groups"));
   }
 
   public void gotoContacts() {
-      wd.findElement(By.xpath("//div/div[4]/div/i/a[2]")).click();
+      click(By.xpath("//div/div[4]/div/i/a[2]"));
   }
 }
