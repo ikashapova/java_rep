59186ffd8c56ab1af7b528e77da88cde3b9b3902
Irina <i.a.kashapova@rambler.ru>
2018-04-13 03:19:11 +0400

    6 задание: создан тест для модификации групп

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase {
+  @Test
+  public void testGroupModification(){
+    app.getNavigationHelper().gotoGroup();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("test2", "test3", "test4"));
+    app.getGroupHelper().submitGroupModification();
+    app.getNavigationHelper().gotoGroup();
+
+
+  }
+
+
+}
