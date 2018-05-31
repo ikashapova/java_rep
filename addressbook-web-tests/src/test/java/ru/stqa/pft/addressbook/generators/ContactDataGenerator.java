965a92d25339919a75b599984dedeac3c0f953be
Irina <i.a.kashapova@rambler.ru>
2018-05-31 18:01:57 +0300

    Задание №12: Реализовать генераторы тестовых данных

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -1,5 +1,9 @@
 package ru.stqa.pft.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import com.thoughtworks.xstream.XStream;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -11,19 +15,61 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ContactDataGenerator {
+  @Parameter(names = "-c", description = "Group count")
+  public int count;
+
+  @Parameter(names = "-f", description = "Target file")
+  public String file;
+
+  @Parameter(names = "-d", description = "Data format")
+  public String format;
+
+
 
   public static void main (String [] args) throws IOException {
-    int count = Integer.parseInt(args[0]);
-    File file = new File(args[1]);
-    List<ContactData> contacts = generateContacts (count);
-    save(contacts, file);
-
+    ContactDataGenerator generator = new ContactDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+    try {jCommander.parse(args);}
+    catch (ParameterException ex){
+      jCommander.usage();
+      return;
+    }
+    generator.run();
 
   }
 
+
+  private void run() throws IOException {
+    List<ContactData> contacts = generateContacts (count);
+    if (format.equals(("csv"))){
+      saveAsScv(contacts, new File(file));
+    }
+    else if (format.equals("xml")){
+      saveAsXml(contacts, new File(file));
+    } else {
+      System.out.println("Unregnized format " + format);
+    }
+  }
+
+  private void saveAsScv(List<ContactData> contacts, File file) throws IOException {
+    System.out.println(new File(".").getAbsolutePath());
+    Writer writer = new FileWriter(file);
+    for (ContactData contact : contacts){
+      writer.write(String.format("%s;%s;%s\n", contact.getFirstname(),contact.getLastname(), contact.getAddress()));
+    }
+    writer.close();
+  }
+
+
+  private void saveAsXml(List<ContactData> contacts, File file) throws IOException {
+    XStream xstream = new XStream();
+    String xml = xstream.toXML(contacts);
+    Writer writer = new FileWriter(file);
+    writer.write(xml);
+    writer.close();
+  }
+
   private static void save(List<ContactData> contacts, File file) throws IOException {
-    //File currentDir = new File(".");
-    //System.out.println(currentDir.getAbsolutePath());
     System.out.println(new File(".").getAbsolutePath());
     Writer writer = new FileWriter(file);
     for (ContactData contact : contacts){
